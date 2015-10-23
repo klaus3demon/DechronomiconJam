0ab899aa56542c3f9668844ba8de1d71e363639b
klaus3demon <franmedida@gmail.com>
2015-10-24 00:39:35 +0200

    escena principal

---
+++ Assets/Scripts/Fran/CargarScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CargarScene : MonoBehaviour {
+	public string scene;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CargaLa(){
+		Application.LoadLevel (scene);
+	}
+}
