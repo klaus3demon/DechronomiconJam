0719b81d73babe5821983b3b563479253ea56628
klaus3demon <franmedida@gmail.com>
2015-10-24 14:50:18 +0200

    Viejo Animado

--- Assets/Scripts/Fran/NewBehaviourScript.cs
+++ Assets/Scripts/Fran/NewBehaviourScript.cs
@@ -4,13 +4,23 @@ using System.Collections;
 public class NewBehaviourScript : MonoBehaviour {
 	public GameObject hola;
 	// Use this for initialization
-	public GameObject spirite;
+	public int speed =1;
+	public float spacing = 10.0f;
+	private Vector3 pos;
 	
 	public void Start() {
+		pos = transform.position;
 	}
 	
 	public void Update() {
-
+		if (Input.GetKey(KeyCode.W))
+			pos.y=1;
+		if (Input.GetKey(KeyCode.S))
+			pos.y -= 1;
+		if (Input.GetKey(KeyCode.A))
+			transform.Translate ((-0.1f),0f,0f) ;
+		if (Input.GetKey(KeyCode.D))
+			transform.Translate (0.1f,0f,0f) ;
 		
 
 	}
