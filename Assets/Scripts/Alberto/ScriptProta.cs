fbd9adea02a3f44c39426e2ef7ce6a5eed785d4c
Zeixen <abuin17@gmail.com>
2015-10-24 14:39:36 +0200

    Hola soy alberto y feliz navidad, y la europea?

--- Assets/Scripts/Alberto/ScriptProta.cs
+++ Assets/Scripts/Alberto/ScriptProta.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class ScriptProta : MonoBehaviour {
     public GameObject player;
     public GameObject bala;
-    public float speed = 0.7f;
+    public float speed = 0.1f;
+    public float rotationSpeed = 100.0F; //Velocidad de rotación
     public float fuerza = 5.0f;
     ForceMode2D mode = ForceMode2D.Force;
     public float spacing = 1.0f;
     public Vector3 pos;
     public bool enaire = false;
+    public bool direccionderecha = true;
 
     // Use this for initialization
     void Start () {
@@ -21,14 +23,17 @@ public class ScriptProta : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.W) && enaire == false)
         {
-            this.GetComponent<Rigidbody2D>().AddForce(transform.up * fuerza);
+            this.GetComponent<Rigidbody2D>().AddForce(transform.up * fuerza);              
             enaire = true;
         }
         if (Input.GetKey(KeyCode.A))
         {
+            //GetComponent<MovimientoProtagonista>().andar();
             if (enaire == false)
             {
-                transform.Translate((-speed), 0f, 0f);
+                transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+                transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0);
+                Debug.Log(-Input.GetAxis("Horizontal"));
             }
             if (enaire == true)
             {
@@ -46,15 +51,24 @@ public class ScriptProta : MonoBehaviour {
                 transform.Translate(0.05f, 0f, 0f);
             }
         }
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            
+            cronosShotAttack();
         }
+        //---------------------- Codigo para que el disparo sea láser dejando pulsado clic
+        /*if (Input.GetKey(KeyCode.Mouse0))
+        {
+            cronosShotAttack();
+        }*/
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.name == "Suelo"){
             enaire = false;
         }
+    }
+    private void cronosShotAttack()
+    {
+        Instantiate(bala, this.transform.localPosition, this.transform.localRotation);
     }
 }
