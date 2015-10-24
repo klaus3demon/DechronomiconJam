31d37175f4339cec6adceb9394c0cc4dcf404ef0
klaus3demon <franmedida@gmail.com>
2015-10-24 16:49:58 +0200

    andamos

---
+++ Assets/Scripts/Fran/MovimientoProtagonista.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoProtagonista : MonoBehaviour {
+	
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void andar(){
+
+			GetComponent<Animator> ().SetBool ("Move", true);
+	
+	}
+	public void noandes(){
+			GetComponent<Animator> ().SetBool ("Move", false);
+		
+	}
+}
