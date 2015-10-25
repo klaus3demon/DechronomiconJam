100eb6de997d699ef22a962768463ab8e8c0cf00
JaimeJesusSerrano <jaime.serranorodriguez@alum.uca.es>
2015-10-25 11:41:11 +0100

    Shot Sistem

---
+++ Assets/Scripts/Jaime/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+
+    private float creationTime;
+    void Start()
+    {
+        creationTime = Time.time;
+        //this.transform.TransformDirection(destine);
+    }
+
+    private int secondsLife = 5;
+    void Update()
+    {
+        this.transform.Translate(Vector3.right);
+        if ( (creationTime + secondsLife) <= Time.time)
+            Destroy(this.gameObject);
+    }
+    
+    private Vector3 destine;
+    //private Vector3 rotation;
+    public void Init(Vector3 destine)
+    {
+        this.destine = destine;
+        //this.rotation = rotation;
+    }
+
+}
+
