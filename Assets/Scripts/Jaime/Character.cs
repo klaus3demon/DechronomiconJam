e910ccc47e632d9755b2f344deefbabe35df9b41
klaus3demon <franmedida@gmail.com>
2015-10-25 13:31:05 +0100

    protamodify

--- Assets/Scripts/Jaime/Character.cs
+++ Assets/Scripts/Jaime/Character.cs
@@ -55,6 +55,7 @@ public class Character : MonoBehaviour {
                 }
                 count_direction--;
             }
+			GetComponent<MovimientoProtagonista>().andar();
             nextMove = Time.time + moveDelay;
         }
 
